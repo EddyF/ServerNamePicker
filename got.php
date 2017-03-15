f89dccbf8cd1bc46b3c87b44286103f3deb36986
Eddy Ferreira <mynameis@eddyf.co.uk>
2017-03-15 21:20:23 +0000

    Update got.php

--- got.php
+++ got.php
@@ -1,85 +1,84 @@
 <?php
 
 $houses = [
-			  [
-				  'label' => 'House Stark',
-				  'img' => 'http://vignette2.wikia.nocookie.net/gameofthrones/images/5/5c/House-Karstark-Main-Shield.PNG/revision/latest?cb=20151209163900',
-				  'description' => 'House Karstark of Karhold is a vassal house that held fealty to House Stark of Winterfell prior to the execution of Lord Rickard Karstark, and briefly did so for House Bolton of the Dreadfort. Their lands are northeast of Winterfell, located in heavy forested lands overlooking the Shivering Sea. Their stronghold is a castle called Karhold and the head of the house is the Lord of Karhold.'
-			  ] ,
-			  [
-				  'label' => 'House Targaryen',
-				  'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest?cb=20170101095357',
-				  'description' => 'House Targaryen is a former Great House of Westeros and was the ruling royal House of the Seven Kingdoms for three centuries, before it was deposed during Roberts Rebellion and House Baratheon replaced it as the new royal House. The few surviving Targaryens fled into exile. Currently based in Essos, House Targaryen seeks to retake the Seven Kingdoms from House Lannister, who formally replaced House Baratheon as the royal House following the destruction of the Great Sept of Baelor.'
-			  ],
+		[
+			'label' => 'House Stark',
+			'img' => 'http://vignette2.wikia.nocookie.net/gameofthrones/images/5/5c/House-Karstark-Main-Shield.PNG/revision/latest?cb=20151209163900',
+			'description' => 'House Karstark of Karhold is a vassal house that held fealty to House Stark of Winterfell prior to the execution of Lord Rickard Karstark, and briefly did so for House Bolton of the Dreadfort. Their lands are northeast of Winterfell, located in heavy forested lands overlooking the Shivering Sea. Their stronghold is a castle called Karhold and the head of the house is the Lord of Karhold.'
+		] ,
+		[
+			'label' => 'House Targaryen',
+			'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest?cb=20170101095357',
+			'description' => 'House Targaryen is a former Great House of Westeros and was the ruling royal House of the Seven Kingdoms for three centuries, before it was deposed during Roberts Rebellion and House Baratheon replaced it as the new royal House. The few surviving Targaryens fled into exile. Currently based in Essos, House Targaryen seeks to retake the Seven Kingdoms from House Lannister, who formally replaced House Baratheon as the royal House following the destruction of the Great Sept of Baelor.'
+		],
 
-			  [
-				  'label' => 'House Lannister',
-				  'img' => '',
-				  'description' => 'House Lannister of Casterly Rock is one of the Great Houses of Westeros, one of its richest and most powerful families and oldest dynasties. It is also the current royal house of the Seven Kingdoms following the extinction of House Baratheon of Kings Landing.'
-			   ],
+		[
+			'label' => 'House Lannister',
+			'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest?cb=20170101095357',
+			'description' => 'House Lannister of Casterly Rock is one of the Great Houses of Westeros, one of its richest and most powerful families and oldest dynasties. It is also the current royal house of the Seven Kingdoms following the extinction of House Baratheon of Kings Landing.'
+		],
+		[
+			'label' => 'Great House',
+			'img' => 'http://vignette4.wikia.nocookie.net/gameofthrones/images/b/ba/Regions_of_Westeros.PNG/revision/latest/scale-to-width-down/180?cb=20160808103139',
+			'description' => 'The Great Houses are the most powerful of the noble houses of the Seven Kingdoms. They exercise immense authority and power over their vassals and territories and are answerable only to the King of the Andals and the First Men, Lord of the Seven Kingdoms and Protector of the Realm.' ,
+		],
 
-			  [
-				  'label' => 'Great House',
-				  'img' => 'http://vignette4.wikia.nocookie.net/gameofthrones/images/b/ba/Regions_of_Westeros.PNG/revision/latest/scale-to-width-down/180?cb=20160808103139',
-				  'description' => 'The Great Houses are the most powerful of the noble houses of the Seven Kingdoms. They exercise immense authority and power over their vassals and territories and are answerable only to the King of the Andals and the First Men, Lord of the Seven Kingdoms and Protector of the Realm.' ,
-			   ],
-
-	 			[
-				  'label' => 'House Tyrell',
-				  'img' => 'http://vignette2.wikia.nocookie.net/gameofthrones/images/c/cf/House-Tyrell-Main-Shield.PNG/revision/latest?cb=20170108163035',
-				  'description' => 'House Tyrell of Highgarden is one of the Great Houses of Westeros. It rules over the Reach, a vast, fertile, and heavily-populated region of southwestern Westeros, from their castle-seat, Highgarden. Formerly led by Lord Mace Tyrell, his mother, the indomitable Olenna Tyrell, stands as the last known member of the house. Maces son Loras was a noted tournament knight (and, secretly, the lover of Lord Renly Baratheon).'
-			   ],
-		   	  [
-				  'label' => 'House Baratheon',
-				  'img' => 'http://vignette4.wikia.nocookie.net/gameofthrones/images/0/00/House-Baratheon-Main-Shield.PNG/revision/latest?cb=20161206005433',
-				  'description' => 'House Baratheon of Storms End is a legally extinct Great House of Westeros. Its main branch was formerly the royal house, but House Lannister now controls the throne.'
-			  ],
-		   	   [
-				   'label' => 'House Bolton',
-				   'img' => 'http://vignette1.wikia.nocookie.net/gameofthrones/images/d/dd/House-Bolton-Main-Shield.PNG/revision/latest?cb=20161231131431',
-				   'description' => 'House Bolton of the Dreadfort is an extinct noble house from the North. After the Red Wedding, they became the Great House of the North, having usurped their position from their former liege lords, House Stark.'
-			  ],
-		   	   [
-		   	   		'label' => 'House Tully',
-					'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/b/bd/House-Tully-Main-Shield.PNG/revision/latest?cb=20160312103126',
-					'description' => 'House Tully of Riverrun is an exiled Great House of Westeros. Its most senior member carried the title of Lord of Riverrun and Lord Paramount of the Trident, until the Red Wedding. The current head is Lord Edmure Tully, son of the late Hoster Tully. The Tully sigil is a silver trout on a red and blue background. Their motto is "Family, Duty, Honor."'
-			   ],
-		   		[
-					'label' => 'House Greyjoy',
-					'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20160312122204',
-					'description' => 'House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands, a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. The head of the house is the Lord Reaper of Pyke.'
-				],
-		   		[
-					'label' => 'House Mormont',
-					'img'	=> 'http://vignette3.wikia.nocookie.net/gameofthrones/images/0/03/House-Mormont-Main-Shield.PNG/revision/latest?cb=20161231120854',
-					'description' => ''
-				],
-				[
-					'label' => 'House Arryn',
-					'img'	=> 'http://vignette3.wikia.nocookie.net/gameofthrones/images/0/03/House-Mormont-Main-Shield.PNG/revision/latest?cb=20161231120854',
-					'description' => 'House Mormont of Bear Island is a vassal house that holds fealty to House Stark of Winterfell. They rule Bear Island, an island far to the north-west of Winterfell. '
-				],
-		   		[
-					'label' => 'House Frey',
-					'img'	=> 'http://vignette1.wikia.nocookie.net/gameofthrones/images/e/e6/House-Frey-Main-Shield.PNG/revision/latest?cb=20160312124244',
-					'description' => 'House Frey of the Twins is a noble house from the Riverlands. They are currently the Great House of the Riverlands, having gained their position for their treachery against their former liege lords, House Tully, who were stripped of all their lands and titles for their rebellion against the Iron Throne'
-				],
-		   		[
-					'label' => 'Noble House',
-					'img'	=> 'http://vignette2.wikia.nocookie.net/gameofthrones/images/a/a7/Noble_Houses.png/revision/latest/scale-to-width-down/250?cb=20140413223305',
-					'description' => 'The Seven Kingdoms of Westeros are divided between many hundreds of noble houses of various sizes. In Westeros ultimate power derives from the King on the Iron Throne and descends through the Great Houses that rule the constituent regions of the continent to their vassals.'
-				],
-				[
-					'label' => 'House Forrester',
-					'img'	=> 'http://vignette2.wikia.nocookie.net/gameofthrones/images/7/7b/House-Forrester-Main-Shield.PNG/revision/latest?cb=20161231145807',
-					'description' => 'House Forrester of Ironrath is an exiled noble house from the Wolfswood in the North of Westeros. They are vassals of the Glovers of Deepwood Motte, as well as staunch Stark bannermen, and have their seat at the wooden castle of Ironrath.'
-				],
-				[
-					'label' => 'House Karstark',
-					'img'	=> 'http://vignette2.wikia.nocookie.net/gameofthrones/images/5/5c/House-Karstark-Main-Shield.PNG/revision/latest?cb=20151209163900',
-					'description' => 'House Karstark of Karhold is a vassal house that held fealty to House Stark of Winterfell prior to the execution of Lord Rickard Karstark, and briefly did so for House Bolton of the Dreadfort.'
-				]
-		   ];
+		[
+			'label' => 'House Tyrell',
+			'img' => 'http://vignette2.wikia.nocookie.net/gameofthrones/images/c/cf/House-Tyrell-Main-Shield.PNG/revision/latest?cb=20170108163035',
+			'description' => 'House Tyrell of Highgarden is one of the Great Houses of Westeros. It rules over the Reach, a vast, fertile, and heavily-populated region of southwestern Westeros, from their castle-seat, Highgarden. Formerly led by Lord Mace Tyrell, his mother, the indomitable Olenna Tyrell, stands as the last known member of the house. Maces son Loras was a noted tournament knight (and, secretly, the lover of Lord Renly Baratheon).'
+		],
+		[
+			'label' => 'House Baratheon',
+			'img' => 'http://vignette4.wikia.nocookie.net/gameofthrones/images/0/00/House-Baratheon-Main-Shield.PNG/revision/latest?cb=20161206005433',
+			'description' => 'House Baratheon of Storms End is a legally extinct Great House of Westeros. Its main branch was formerly the royal house, but House Lannister now controls the throne.'
+		],
+		[
+			'label' => 'House Bolton',
+			'img' => 'http://vignette1.wikia.nocookie.net/gameofthrones/images/d/dd/House-Bolton-Main-Shield.PNG/revision/latest?cb=20161231131431',
+			'description' => 'House Bolton of the Dreadfort is an extinct noble house from the North. After the Red Wedding, they became the Great House of the North, having usurped their position from their former liege lords, House Stark.'
+		],
+		[
+			'label' => 'House Tully',
+			'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/b/bd/House-Tully-Main-Shield.PNG/revision/latest?cb=20160312103126',
+			'description' => 'House Tully of Riverrun is an exiled Great House of Westeros. Its most senior member carried the title of Lord of Riverrun and Lord Paramount of the Trident, until the Red Wedding. The current head is Lord Edmure Tully, son of the late Hoster Tully. The Tully sigil is a silver trout on a red and blue background. Their motto is "Family, Duty, Honor."'
+		],
+		[
+			'label' => 'House Greyjoy',
+			'img' => 'http://vignette3.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20160312122204',
+			'description' => 'House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands, a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. The head of the house is the Lord Reaper of Pyke.'
+		],
+		[
+			'label' => 'House Mormont',
+			'img'	=> 'http://vignette3.wikia.nocookie.net/gameofthrones/images/0/03/House-Mormont-Main-Shield.PNG/revision/latest?cb=20161231120854',
+			'description' => 'House Mormont of Bear Island is a vassal house that holds fealty to House Stark of Winterfell. They rule Bear Island, an island far to the north-west of Winterfell. The head of the house is the Lord of Bear Island. The family possessed the Valyrian sword Longclaw, which later was given to Jon Snow.'
+		],
+		[
+			'label' => 'House Arryn',
+			'img'	=> 'http://vignette3.wikia.nocookie.net/gameofthrones/images/0/03/House-Mormont-Main-Shield.PNG/revision/latest?cb=20161231120854',
+			'description' => 'House Mormont of Bear Island is a vassal house that holds fealty to House Stark of Winterfell. They rule Bear Island, an island far to the north-west of Winterfell. '
+		],
+		[
+			'label' => 'House Frey',
+			'img'	=> 'http://vignette1.wikia.nocookie.net/gameofthrones/images/e/e6/House-Frey-Main-Shield.PNG/revision/latest?cb=20160312124244',
+			'description' => 'House Frey of the Twins is a noble house from the Riverlands. They are currently the Great House of the Riverlands, having gained their position for their treachery against their former liege lords, House Tully, who were stripped of all their lands and titles for their rebellion against the Iron Throne'
+		],
+		[
+			'label' => 'Noble House',
+			'img'	=> 'http://vignette2.wikia.nocookie.net/gameofthrones/images/a/a7/Noble_Houses.png/revision/latest/scale-to-width-down/250?cb=20140413223305',
+			'description' => 'The Seven Kingdoms of Westeros are divided between many hundreds of noble houses of various sizes. In Westeros ultimate power derives from the King on the Iron Throne and descends through the Great Houses that rule the constituent regions of the continent to their vassals.'
+		],
+		[
+			'label' => 'House Forrester',
+			'img'	=> 'http://vignette2.wikia.nocookie.net/gameofthrones/images/7/7b/House-Forrester-Main-Shield.PNG/revision/latest?cb=20161231145807',
+			'description' => 'House Forrester of Ironrath is an exiled noble house from the Wolfswood in the North of Westeros. They are vassals of the Glovers of Deepwood Motte, as well as staunch Stark bannermen, and have their seat at the wooden castle of Ironrath.'
+		],
+		[
+			'label' => 'House Karstark',
+			'img'	=> 'http://vignette2.wikia.nocookie.net/gameofthrones/images/5/5c/House-Karstark-Main-Shield.PNG/revision/latest?cb=20151209163900',
+			'description' => 'House Karstark of Karhold is a vassal house that held fealty to House Stark of Winterfell prior to the execution of Lord Rickard Karstark, and briefly did so for House Bolton of the Dreadfort.'
+		]
+];
 
 $devs = [ 'Dave' , 'Eddy' , 'Johny' , 'Noel' ];
 
